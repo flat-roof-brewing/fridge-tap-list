8b5039d48514f0c8e785552a865cb4d24d8dbd4b
Linus Unnebäck <linus@folkdatorn.se>
2023-02-17 11:31:58 +0700

    🐛 Use all available heap

--- src/main.rs
+++ src/main.rs
@@ -46,15 +46,16 @@ extern crate alloc;
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-
     extern "C" {
         static mut _heap_start: u32;
+        static mut _heap_end: u32;
     }
 
     unsafe {
         let heap_start = &_heap_start as *const _ as usize;
-        ALLOCATOR.init(heap_start as *mut u8, HEAP_SIZE);
+        let heap_end = &_heap_end as *const _ as usize;
+
+        ALLOCATOR.init(heap_start as *mut u8, heap_end - heap_start);
     }
 }
 
