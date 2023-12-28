331bc2a892ea4f3b987ca944d0c634bb5daaa7dd
Linus Unnebäck <linus@folkdatorn.se>
2023-12-28 15:27:44 +0100

    🎉 Add LM75 temperature sensor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #![no_std]
 #![no_main]
 
+mod lm75;
 mod st7032;
+
+use core::fmt::Write;
 
 use esp_backtrace as _;
 use esp_println::println;
 use hal::{clock::ClockControl, i2c::I2C, peripherals::Peripherals, prelude::*, Delay, IO};
+use shared_bus::BusManagerSimple;
 
-use crate::st7032::ST7032;
+use crate::{st7032::ST7032, lm75::LM75};
 
 #[entry]
 fn main() -> ! {
@@ -33,9 +37,12 @@ fn main() -> ! {
         &clocks,
     );
 
+    let i2c = BusManagerSimple::new(i2c);
+
     println!("Hello world!");
 
-    let mut lcd = ST7032::new(i2c);
+    let mut sensor = LM75::new(i2c.acquire_i2c());
+    let mut lcd = ST7032::new(i2c.acquire_i2c());
 
     lcd.init().unwrap();
     delay.delay_ms(500u32);
@@ -43,7 +50,9 @@ fn main() -> ! {
     lcd.set_line(0, "Jul 2023").unwrap();
 
     loop {
-        println!("Loop...");
-        delay.delay_ms(500u32);
+        let temp = sensor.measure().unwrap();
+        lcd.set_cursor(9, 1).unwrap();
+        write!(lcd, "{: >5.1} C", temp).unwrap();
+        delay.delay_ms(2500u32);
     }
 }
